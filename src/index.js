86f1a0f4857d16f2494bc016b7c594e51f9ec137
psgivens <psgivens11@yahoo.com>
2016-12-08 05:37:38 -0800

    Touching up the git project.

--- src/index.js
+++ src/index.js
@@ -1,2 +1,6 @@
 
-console.log("Hello World");
+console.log("Welcome to the new world of JavaScript programming");
+
+console.log("Please review react-redux-router-es6-todo for a D3 example.");
+
+console.log("TODO: Createa a D3 gallery project on GitHub");
