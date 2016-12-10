2e11ca6affa84ee177f67bc9ee289d6f84d2f06b
psgivens <psgivens11@yahoo.com>
2016-12-10 07:49:07 -0800

    Added a D3 histogram

--- wd3/src/index.js
+++ wd3/src/index.js
@@ -33,14 +33,51 @@ var svg = d3
     .attr("y", function (d) {
       return h - (d * 4);
     })
-//      .attr("class", "bar")
     .attr("width", w / dataset.length - barPadding)
     .attr("height", function (d) {
       return d * 4;
     })
     .attr("fill", function (d) {
       return "rgb(0, 0, " + (d * 10) +")";
-    });
+    })
+    .on("mouseover", function (d) {
+      var xPosition = parseFloat(d3.select(this).attr("x"));// + (w / dataset.length) / 2;
+      var yPosition = parseFloat(d3.select(this).attr("y")) + 14;
+
+      svg.append("rect")
+        .attr("id", "tooltipbox")
+        .attr("x", xPosition)
+        .attr("y", yPosition - 10)
+        .attr("width", 20)
+        .attr("height", 15)
+        .attr("fill", "yellow")
+        .text(d);
+
+      svg.append("text")
+        .attr("id", "tooltip")
+        .attr("x", xPosition)
+        .attr("y", yPosition)
+        .attr("text-anchor", "left")
+        .attr("font-family", "sans-serif")
+        .attr("font-size", "11px")
+        .attr("font-weight", "bold")
+        .attr("fill", "black")
+        .text(d);
+
+      d3.select(this)
+      .attr("fill", "orange");
+    })
+    .on("mouseout", function (d) {
+
+        d3.select("#tooltip").remove();
+        d3.select("#tooltipbox").remove();
+
+        d3.select (this)
+          .transition ().duration (750)
+          .attr("fill", "rgb(0, 0, " + (d * 10) + ")");
+    })
+    ;
+
   svg.selectAll("text")
      .data(dataset)
      .enter ()
